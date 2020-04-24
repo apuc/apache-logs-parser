767a559849528057ab1f5c68e8c3d8365154479e
Kavalar <apuc06@mail.ru>
2020-04-25 01:23:50 +0300

    first

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirill
+ * Date: 24.04.20
+ * Time: 23:54
+ */
+
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . '/lib/ApacheParser.php';
+require_once __DIR__ . '/lib/ApacheParserError.php';
+
+$file = new \lib\ApacheParserError(__DIR__ . '/error.log');
+
+echo "<pre>";
+print_r($file->parse()->getLogs());
+echo "</pre>";
---
+++ lib/ApacheParserAccess.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirill
+ * Date: 25.04.20
+ * Time: 1:19
+ */
+
+namespace lib;
+
+
+class ApacheParserAccess extends ApacheParser
+{
+
+    public function parse()
+    {
+        // TODO: parse access method
+    }
+
+}
